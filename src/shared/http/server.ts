8dd8979b7294df37faf65ce5f0d3deee682fd0b4
hencabral <lhenriquezan@gmail.com>
2023-01-04 14:43:38 -0300

    module products

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,12 +1,33 @@
-import express from 'express';
-import cors from 'cors';
-import routes from './routes';
+import 'reflect-metadata'
+import express, { NextFunction, Request, Response } from 'express'
+import 'express-async-errors'
+import cors from 'cors'
+import { errors } from 'celebrate'
+import routes from './routes'
+import AppError from '@shared/errors/AppError'
+import '@shared/typeorm'
 
-const app = express();
+const app = express()
 
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
-app.use(routes);
+app.use(routes)
 
-app.listen(3333, () => console.log('Server started on port 3333!'));
+app.use(errors())
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    })
+  }
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal Server Error',
+  })
+})
+
+app.listen(3333, () => console.log('Server started on port 3333!'))
