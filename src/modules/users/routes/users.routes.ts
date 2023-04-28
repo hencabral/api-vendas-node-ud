3a0a0341d9900fe50b20ec1ba194b0366111e95d
hencabral <lhenriquezan@gmail.com>
2023-04-28 16:30:24 -0300

    feat: create route updateUserAvatar

--- src/modules/users/routes/users.routes.ts
+++ src/modules/users/routes/users.routes.ts
@@ -1,11 +1,17 @@
 import { Router } from 'express'
 import UsersController from '../controllers/UsersController'
 import { celebrate, Joi, Segments } from 'celebrate'
+import multer from 'multer'
+import uploadConfig from '@config/upload'
 import isAuthenticated from '../../../shared/http/middlewares/isAuthenticated'
+import UserAvatarController from '../controllers/UserAvatarController'
 
 const usersRouter = Router()
 
 const usersController = new UsersController()
+const usersAvatarController = new UserAvatarController()
+
+const upload = multer(uploadConfig)
 
 usersRouter.get('/', isAuthenticated, usersController.index)
 
@@ -55,6 +61,13 @@ usersRouter.delete(
     },
   }),
   usersController.delete,
+)
+
+usersRouter.patch(
+  '/avatar',
+  isAuthenticated,
+  upload.single('avatar'),
+  usersAvatarController.update,
 )
 
 export default usersRouter
