cc254d5b081a2615d3885aebee5ddef40103de50
hencabral <lhenriquezan@gmail.com>
2023-05-10 12:43:59 -0300

    feat: add forgotPasswordTemplate

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,5 +1,6 @@
 import AppError from '@shared/errors/AppError'
 import { getCustomRepository } from 'typeorm'
+import path from 'path'
 import UsersRepository from '../typeorm/repositories/UserRepository'
 import UserTokensRepository from '../typeorm/repositories/UserTokensRepository'
 import EtherealMail from '@config/mail/EtherealMail'
@@ -19,7 +20,14 @@ class SendForgotPasswordEmailService {
     }
 
     const { token } = await userTokensRepository.generate(user.id)
-    //console.log(token)
+
+    const forgotPasswordTemplate = path.resolve(
+      __dirname,
+      '..',
+      'views',
+      'forgot_password.hbs',
+    )
+
     await EtherealMail.sendMail({
       to: {
         name: user.name,
@@ -27,10 +35,10 @@ class SendForgotPasswordEmailService {
       },
       subject: '[API Vendas] Recuperação de Senha',
       templateData: {
-        template: `Olá {{name}}: {{token}}`,
+        file: forgotPasswordTemplate,
         variables: {
           name: user.name,
-          token,
+          link: `http:localhost:3000/reset_password?token=${token}`,
         },
       },
     })
